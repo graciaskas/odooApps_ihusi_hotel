eb56e5d1d82ac4269585198f5a5d61ac80e7b80f
graciaskas <makaveligracias@aol.com>
2023-11-15 04:52:59 +0200

    updated

---
+++ uzima_bora/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name":"Uzima Bora Sarl",
+    "description":"Module pour personnalisation Uzima Bora",
+    "depends":['base','sale','sale_management','purchase','account'],
+    "data":[
+        "reports/sale_report_document.xml",
+        "reports/purchase_report_document.xml"
+    ],
+    "website":"https://zeslap.com",
+    "author":"Gracias Kasongo",
+    "auto_install":False
+}
